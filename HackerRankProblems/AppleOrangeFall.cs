32ea737e05c12b9fe53eec4164fad285292725c2
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-05-21 16:35:46 +0300

    Kangaroo task and tests were added

---
+++ HackerRankProblems/AppleOrangeFall.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems
+{
+    public class AppleOrangeFall
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s">House left position</param>
+        /// <param name="t">House right position</param>
+        /// <param name="a">Apple tree position</param>
+        /// <param name="b">Orange tree position</param>
+        /// <param name="apples">Apples fall distance</param>
+        /// <param name="oranges">Oranges fall distance</param>
+        public static void CountApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            int fallApplesCount = 0;
+            int fallOrangesCount = 0;
+
+            foreach (var apple in apples)
+            {
+                if (apple > 0 && (a + apple >= s && a + apple <= t))
+                {
+                    fallApplesCount++;
+                }
+            }
+
+            foreach (var orange in oranges)
+            {
+                if (orange < 0 && (b + orange <= t && b + orange >= s))
+                {
+                    fallOrangesCount++;
+                }
+            }
+
+            //Console.WriteLine(fallApplesCount);
+            //Console.WriteLine(fallOrangesCount);
+        }
+
+    }
+}
---
+++ HackerRankProblems/Kangaroo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems
+{
+    public class Kangaroo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="v1"></param>
+        /// <param name="x2"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static string CanMeetAtOnePoint(int x1, int v1, int x2, int v2)
+        {
+            if (v1 < v2) return "NO";
+
+            while (x1 <= x2)
+            {
+                x1 += v1;
+                x2 += v2;
+
+                if (x1 == x2) return "YES";
+            }
+
+            return "NO";
+        }
+    }
+}
