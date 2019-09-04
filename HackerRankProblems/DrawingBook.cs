94aac36bf5741098166e25d707bce0c4d475386a
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-09-04 15:55:58 +0300

    Drawing Book problem and solution were added.

---
+++ HackerRankProblems/DrawingBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems
+{
+    public static class DrawingBook
+    {
+        public static int PageCount(int n, int p)
+        {
+            int result = 0;
+            if (p == 1 || p == n)
+                return result;
+            if (p == n - 1 && n % 2 == 0)
+                return 1;
+            if (p <= n/2)
+            { 
+                for (int i = 2; i <= p; i += 2)
+                {
+                    result++;                    
+                }
+                return result;
+            }         
+            else
+            {
+                int res = n % 2 == 0 ? n - 1 : n - 2;
+
+                for (int i = res; i >= p; i -= 2)
+                {
+                    result++;
+                }
+                return result;
+            }
+        }
+    }
+}
--- HackerRankTests/TDD.cs
+++ HackerRankTests/TDD.cs
@@ -334,6 +334,29 @@ namespace HackerRankTests
             Assert.AreEqual(expectedRes, result);
         }
 
-        
+        [Test]
+        [TestCase(6, 2, 1)]        
+        [TestCase(6, 1, 0)]
+        [TestCase(6, 3, 1)]
+        [TestCase(7, 2, 1)]
+        [TestCase(6, 4, 1)]
+        [TestCase(7, 4, 1)]
+        [TestCase(7, 7, 0)]
+        [TestCase(6, 6, 0)]
+        [TestCase(8, 6, 1)]
+        [TestCase(8, 5, 2)]
+        [TestCase(6, 5, 1)]
+        public void DrawingBookTest(int n, int p, int expectedResult)
+        {
+            //Arrange
+
+            //Act
+            int result = DrawingBook.PageCount(n, p);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
     }
 }
