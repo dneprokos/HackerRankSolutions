8f9eeb3cb891f40cd9828250c29184c5872adcc2
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-10-30 07:52:37 +0200

    Add new problem resolving

---
+++ HackerRankProblems/Implementations/BirthdayChocolate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems.Implementations
+{
+    public class BirthdayChocolate
+    {
+        public static int Birthday(List<int> s, int d, int m)
+        {
+            int ways = 0;
+            int maxLength = s.Count == 1 ? 1 : s.Count - m + 1;
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                var sum = 0;
+                for (int k = i; k < i + m; k++)
+                {
+                    sum += s[k];
+                }
+
+                if (sum == d) ways++; 
+            }
+
+            return ways;
+        }
+    }
+}
---
+++ HackerRankProblems/Implementations/BreackingRecords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankProblems.Implementations
+{
+    public class BreackingRecords
+    {
+        public static int[] BreakingRecords(int[] scores)
+        {
+            int[] records = new int[2] { 0, 0 };
+
+            int minScore = scores[0]; 
+            int maxScore = scores[0];
+
+            for (int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] > maxScore)
+                {
+                    maxScore = scores[i];
+                    records[0]++;
+                    continue;
+                }
+                if (scores[i] < minScore)
+                {
+                    minScore = scores[i];
+                    records[1]++;
+                }
+            }
+            return records;
+        }
+    }
+}
