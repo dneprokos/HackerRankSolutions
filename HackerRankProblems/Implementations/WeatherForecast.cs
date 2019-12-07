1b3632e6acf08698c63d6b76f393493c4ab1856f
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-12-07 10:35:52 +0200

    WetherForecast problem and tests were added

---
+++ HackerRankProblems/Implementations/WeatherForecast.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRankProblems.Implementations
+{
+    public class WeatherForecast
+    {
+        public enum WeekDay
+        {
+            Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+        }
+
+        public static string GetDayWithTemperatureNearToWeekAverage(int [] weekdayTemperatures)
+        {
+            var expectedDay = string.Empty;
+            var minTempDiff = 0;
+            int averageWeekTemp = weekdayTemperatures.Sum()/weekdayTemperatures.Count();
+
+            for (int i = 0; i < weekdayTemperatures.Length; i++)
+            {
+                if (weekdayTemperatures[i] == averageWeekTemp)
+                {
+                    expectedDay = Enum.GetName(typeof(WeekDay), i);
+                    Console.WriteLine("Target day: " + expectedDay);
+                    return expectedDay;
+                }
+
+                int tempDiff = Math.Abs(averageWeekTemp - weekdayTemperatures[i]);
+                if (i == 0) minTempDiff = tempDiff;
+                else if (tempDiff < minTempDiff)
+                {
+                    minTempDiff = tempDiff;
+                    expectedDay = Enum.GetName(typeof(WeekDay), i);
+                } 
+            }
+            
+            Console.WriteLine("Target day: " + expectedDay);
+            return expectedDay;
+        }
+    }
+}
---
+++ HackerRankTests/Tests/WetherForecastTests.cs
@@ -0,0 +1,28 @@
+﻿using HackerRankProblems.Implementations;
+using HackerRankTests.Infrostructure;
+using NUnit.Framework;
+
+namespace HackerRankTests.Tests
+{
+    public class WetherForecastTests:TestBase
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 1, 1, 1, 1, 1 }, "Sunday")]
+        [TestCase(new int[] { 3, 1, 0, 2, 1, 2, 5 }, "Wednesday")]
+        [TestCase(new int[] { -1, 2, 2, 2, 2, 2, 2 }, "Monday")]
+        [TestCase(new int[] { -1, 0, -1, 2, 2, 2, 2 }, "Monday")]
+        [TestCase(new int[] { -3, -1, -2, -2, -2, -2, -2 }, "Tuesday")]
+        [TestCase(new int[] { -3, -1, -2, 2, 2, 2, 2 }, "Monday")]
+        [TestCase(new int[] { -5, 5, -5, 2, 2, 2, 2 }, "Wednesday")]
+        public void GetDayWithTemperatureNearToWeekAverage_ShouldReturnDayString(int [] weekTemperature, string expectedDay)
+        {
+            //Arrange
+
+            //Act
+            string actualDay = WeatherForecast.GetDayWithTemperatureNearToWeekAverage(weekTemperature);
+
+            //Assert
+            Assert.AreEqual(expectedDay, actualDay);
+        }
+    }
+}
