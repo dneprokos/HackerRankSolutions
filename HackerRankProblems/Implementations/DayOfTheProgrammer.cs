46d5190d1414ddfc6ffb7faafcc2c3e34a359633
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2020-02-13 11:27:46 +0200

    New chalanges were solved

---
+++ HackerRankProblems/Implementations/DayOfTheProgrammer.cs
@@ -0,0 +1,114 @@
+﻿using System.Globalization;
+
+namespace HackerRankProblems.Implementations
+{
+    public class DayOfTheProgrammer
+    {
+        #region Public methods
+
+        public static string DayOfProgrammer(int year)
+        {
+            if (year == 1918) 
+                return "26.09.1918";
+
+            if (IsLeapYear(year))
+            {
+                return $"12.09.{year}";
+            } 
+            else
+            {
+                return $"13.09.{year}";
+            }
+        }
+
+        public static string GetDateByDayNumber(int dayNumber, int year)
+        {
+            string date = string.Empty;
+            bool isLeap = IsLeapYear(year);
+
+            if (year == 1918)
+            {
+                string dateAndMonth = GetDateByDayAndMonthAtTransferYear(dayNumber);
+                return $"{dateAndMonth}.{year}";
+            }
+                
+
+            if ((isLeap && dayNumber > 366) 
+                || (!isLeap && dayNumber > 365)
+                || dayNumber < 1) 
+                    return date;
+
+            int[] daysPerMonth = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (isLeap)
+                daysPerMonth[1] += 1;
+
+            string dayAndMonth = CalculateAndReturnDayAndMonth(dayNumber, daysPerMonth);
+            return dayAndMonth != string.Empty ? $"{dayAndMonth}.{year}": date;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static string AddZeroChar(int value)
+        {
+            string result = value.ToString();
+            if (result.Length < 2)
+                result = "0" + value;
+
+            return result;
+        }
+
+        /// <summary>
+        /// 1918 year was a transfer year in Russia from Julian Calendar to Gregorian
+        /// This year February was started from 14 and it wasn't a leap year
+        /// </summary>
+        /// <param name="dayNumber"></param>
+        /// <returns></returns>
+        private static string GetDateByDayAndMonthAtTransferYear(int dayNumber)
+        {
+            //If february 
+            if (dayNumber > 31 && dayNumber < 47)
+            {
+                int dayDiff = 18;
+                return $"{dayNumber - dayDiff}.02"; 
+            }
+            //If any execept february
+            else
+            {
+                int[] daysPerMonth = new int[12] { 31, 15, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                return CalculateAndReturnDayAndMonth(dayNumber, daysPerMonth);
+            }
+        }
+
+        private static bool IsLeapYear(int year) 
+        {
+            if (year >= 1919)
+                //return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+                return new GregorianCalendar().IsLeapYear(year);
+            if (year <= 1917)
+               //return (year % 4 == 0);
+               return new JulianCalendar().IsLeapYear(year, JulianCalendar.JulianEra);
+            return false;           
+        }
+
+        private static string CalculateAndReturnDayAndMonth(int dayNumber, int[] daysPerMonth)
+        {
+            for (int i = 0; i < daysPerMonth.Length; i++)
+            {
+                if (dayNumber <= daysPerMonth[i])
+                {
+                    string dayNumberAsString = AddZeroChar(dayNumber);
+                    string monthAsString = AddZeroChar(i + 1);
+
+                    return $"{dayNumberAsString}.{monthAsString}";
+                }
+                dayNumber -= daysPerMonth[i];
+            }
+            return string.Empty;
+        }
+
+        #endregion
+    }
+}
---
+++ HackerRankTests/Tests/DayOfTheProgrammerTests.cs
@@ -0,0 +1,55 @@
+﻿using HackerRankProblems.Implementations;
+using NUnit.Framework;
+
+namespace HackerRankTests.Tests
+{
+    public class DayOfTheProgrammerTests
+    {
+        [Test]
+        [TestCase(1700, "12.09.1700")]
+        [TestCase(2700, "13.09.2700")]
+        [TestCase(1918, "26.09.1918")]
+        [TestCase(2016, "12.09.2016")]    
+        [TestCase(2018, "13.09.2018")]
+        [TestCase(1800, "12.09.1800")]
+        public void DayOfProgrammerTest(int year, string expectedResult)
+        {
+            //Arrange
+
+            //Act
+            string actual = DayOfTheProgrammer.DayOfProgrammer(year);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [Test]
+        [TestCase(1, 2020, "01.01.2020")]
+        [TestCase(366, 2020, "31.12.2020")]
+        [TestCase(365, 2019, "31.12.2019")]
+        [TestCase(59, 2019, "28.02.2019")]
+        [TestCase(60, 2020, "29.02.2020")]
+        [TestCase(1, 1918, "01.01.1918")]
+        [TestCase(366, 2019, "")]
+        [TestCase(367, 2020, "")]
+        [TestCase(0, 2020, "")]
+        [TestCase(-1, 2020, "")]
+        [TestCase(1, 1918, "01.01.1918")]
+        [TestCase(32, 1918, "14.02.1918")]
+        [TestCase(46, 1918, "28.02.1918")]
+        [TestCase(47, 1918, "01.03.1918")]
+        [TestCase(60, 1900, "29.02.1900")]
+        [TestCase(60, 1889, "01.03.1889")]
+        public void GetDateByDayNumberTest(int dayNumber, int year, string expectedResult)
+        {
+            //Arrange
+
+            //Act
+            string actual = DayOfTheProgrammer.GetDateByDayNumber(dayNumber, year);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+    }
+}
