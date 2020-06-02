73f33d64ebcab4163adc3bfa85d5ed8676d64c98
Teltov <kostiantyn.teltov.ext@dematic.com>
2020-06-02 09:11:05 +0300

    Alternating chars challenge was implemented

---
+++ HackerRankProblems/Implementations/InterviewPreparationKit/Strings/AlternatingCharactersChallenge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HackerRankProblems.Implementations.InterviewPreparationKit.Strings
+{
+    public class AlternatingCharactersChallenge
+    {
+        public static int AlternatingCharacters(string s)
+        {
+            int deletionsCount = 0;
+            Char[] chars = s.ToCharArray();
+            Char curChar = chars[0];
+
+            for (int i = 1; i < chars.Length; i++)
+            {
+                if (chars[i] == curChar) 
+                {
+                    deletionsCount += 1;
+                }
+                else
+                {
+                    curChar = chars[i];
+                }
+
+            }
+            return deletionsCount;
+        }
+
+    }
+}
---
+++ HackerRankTests/Tests/InterviewPreparationKit/Strings/AlternatingCharactersChallengeTests.cs
@@ -0,0 +1,27 @@
+﻿using HackerRankProblems.Implementations.InterviewPreparationKit.Strings;
+using HackerRankTests.Infrostructure;
+using NUnit.Framework;
+
+namespace HackerRankTests.Tests.InterviewPreparationKit.Strings
+{
+    public class AlternatingCharactersChallengeTests: TestBase
+    {
+        [Test]
+        [TestCase("AAAA", 3)]
+        [TestCase("BBBBB", 4)]
+        [TestCase("ABABABAB", 0)]
+        [TestCase("BABABA", 0)]
+        [TestCase("AAABBB", 4)]
+        public void AlternatingCharacters_ShouldReturnMinmumDeletions(string chars, int expectedRes) 
+        {
+            //Arrange
+
+            //Act
+            int actualResult = AlternatingCharactersChallenge.AlternatingCharacters(chars);
+
+            //Assert
+            Assert.AreEqual(expectedRes, actualResult);
+        }
+
+    }
+}
